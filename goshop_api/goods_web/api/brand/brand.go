29212d5b251787ff18912b7a62358fe52d6a6c9f
zhijun zou <zzjbattlefield@aliyun.com>
2021-04-30 17:28:20 +0800

    完成grpc客户端品牌相关的内容

--- goshop_api/goods_web/api/brand/brand.go
+++ goshop_api/goods_web/api/brand/brand.go
@@ -3,27 +3,241 @@ package brand
 import (
 	"context"
 	"goshop_api/goods_web/api"
+	"goshop_api/goods_web/forms"
 	"goshop_api/goods_web/global"
 	"goshop_api/goods_web/proto"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func BrandList(ctx *gin.Context) {
-	if rsp, err := global.GoodsClinet.BrandList(context.Background(), &proto.BrandFilterRequest{Pages: 15, PagePerNums: 1}); err != nil {
+	pn := ctx.DefaultQuery("pn", "0")
+	pnInt, _ := strconv.Atoi(pn)
+	pSize := ctx.DefaultQuery("psize", "10")
+	pSizeInt, _ := strconv.Atoi(pSize)
+
+	rsp, err := global.GoodsClinet.BrandList(context.Background(), &proto.BrandFilterRequest{
+		Pages:       int32(pnInt),
+		PagePerNums: int32(pSizeInt),
+	})
+
+	if err != nil {
 		api.HandleGrpcErrorToHttp(err, ctx)
 		return
-	} else {
-		brandInfo := make([]map[string]interface{}, 0)
-		for _, brand := range rsp.Data {
-			brandInfo = append(brandInfo, map[string]interface{}{
-				"id":   brand.Id,
-				"name": brand.Name,
-				"logo": brand.Logo,
-			})
-		}
-		ctx.JSON(http.StatusOK, gin.H{"data": brandInfo, "total": rsp.Total})
 	}
 
+	result := make([]interface{}, 0)
+	reMap := make(map[string]interface{})
+	reMap["total"] = rsp.Total
+	for _, value := range rsp.Data[pnInt : pnInt*pSizeInt+pSizeInt] {
+		reMap := make(map[string]interface{})
+		reMap["id"] = value.Id
+		reMap["name"] = value.Name
+		reMap["logo"] = value.Logo
+
+		result = append(result, reMap)
+	}
+
+	reMap["data"] = result
+
+	ctx.JSON(http.StatusOK, reMap)
+}
+
+func NewBrand(ctx *gin.Context) {
+	brandForm := forms.BrandForm{}
+	if err := ctx.ShouldBindJSON(&brandForm); err != nil {
+		api.HandleValidatorError(err, ctx)
+		return
+	}
+
+	rsp, err := global.GoodsClinet.CreateBrand(context.Background(), &proto.BrandRequest{
+		Name: brandForm.Name,
+		Logo: brandForm.Logo,
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	request := make(map[string]interface{})
+	request["id"] = rsp.Id
+	request["name"] = rsp.Name
+	request["logo"] = rsp.Logo
+
+	ctx.JSON(http.StatusOK, request)
+}
+
+func DeleteBrand(ctx *gin.Context) {
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+	_, err = global.GoodsClinet.DeleteBrand(context.Background(), &proto.BrandRequest{Id: int32(i)})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	ctx.Status(http.StatusOK)
+}
+
+func UpdateBrand(ctx *gin.Context) {
+	brandForm := forms.BrandForm{}
+	if err := ctx.ShouldBindJSON(&brandForm); err != nil {
+		api.HandleValidatorError(err, ctx)
+		return
+	}
+
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+
+	_, err = global.GoodsClinet.UpdateBrand(context.Background(), &proto.BrandRequest{
+		Id:   int32(i),
+		Name: brandForm.Name,
+		Logo: brandForm.Logo,
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	ctx.Status(http.StatusOK)
+}
+
+func GetCategoryBrandList(ctx *gin.Context) {
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+
+	rsp, err := global.GoodsClinet.GetCategoryBrandList(context.Background(), &proto.CategoryInfoRequest{
+		Id: int32(i),
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	result := make([]interface{}, 0)
+	for _, value := range rsp.Data {
+		reMap := make(map[string]interface{})
+		reMap["id"] = value.Id
+		reMap["name"] = value.Name
+		reMap["logo"] = value.Logo
+
+		result = append(result, reMap)
+	}
+
+	ctx.JSON(http.StatusOK, result)
+}
+
+func CategoryBrandList(ctx *gin.Context) {
+	//所有的list返回的数据结构
+	/*
+		{
+			"total": 100,
+			"data":[{},{}]
+		}
+	*/
+	rsp, err := global.GoodsClinet.CategoryBrandList(context.Background(), &proto.CategoryBrandFilterRequest{})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	reMap := map[string]interface{}{
+		"total": rsp.Total,
+	}
+
+	result := make([]interface{}, 0)
+	for _, value := range rsp.Data {
+		reMap := make(map[string]interface{})
+		reMap["id"] = value.Id
+		reMap["category"] = map[string]interface{}{
+			"id":   value.Category.Id,
+			"name": value.Category.Name,
+		}
+		reMap["brand"] = map[string]interface{}{
+			"id":   value.Brand.Id,
+			"name": value.Brand.Name,
+			"logo": value.Brand.Logo,
+		}
+
+		result = append(result, reMap)
+	}
+
+	reMap["data"] = result
+	ctx.JSON(http.StatusOK, reMap)
+}
+
+func NewCategoryBrand(ctx *gin.Context) {
+	categoryBrandForm := forms.CategoryBrandForm{}
+	if err := ctx.ShouldBindJSON(&categoryBrandForm); err != nil {
+		api.HandleValidatorError(err, ctx)
+		return
+	}
+
+	rsp, err := global.GoodsClinet.CreateCategoryBrand(context.Background(), &proto.CategoryBrandRequest{
+		CategoryId: int32(categoryBrandForm.CategoryId),
+		BrandId:    int32(categoryBrandForm.BrandId),
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	response := make(map[string]interface{})
+	response["id"] = rsp.Id
+
+	ctx.JSON(http.StatusOK, response)
+}
+
+func UpdateCategoryBrand(ctx *gin.Context) {
+	categoryBrandForm := forms.CategoryBrandForm{}
+	if err := ctx.ShouldBindJSON(&categoryBrandForm); err != nil {
+		api.HandleValidatorError(err, ctx)
+		return
+	}
+
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+
+	_, err = global.GoodsClinet.UpdateCategoryBrand(context.Background(), &proto.CategoryBrandRequest{
+		Id:         int32(i),
+		CategoryId: int32(categoryBrandForm.CategoryId),
+		BrandId:    int32(categoryBrandForm.BrandId),
+	})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+	ctx.Status(http.StatusOK)
+}
+
+func DeleteCategoryBrand(ctx *gin.Context) {
+	id := ctx.Param("id")
+	i, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		ctx.Status(http.StatusNotFound)
+		return
+	}
+	_, err = global.GoodsClinet.DeleteCategoryBrand(context.Background(), &proto.CategoryBrandRequest{Id: int32(i)})
+	if err != nil {
+		api.HandleGrpcErrorToHttp(err, ctx)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, "")
 }
